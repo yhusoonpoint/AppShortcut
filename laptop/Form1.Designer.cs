3511eb035bd9fc369f620a5921330284f0b70d99
yhusoonpoint <allinsonadebambo@gmail.com>
2024-02-06 10:57:59 +0000

    Add files via upload

---
+++ laptop/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace laptop
+{
+    partial class D_V_Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(D_V_Launcher));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(448, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 10);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "MIN";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.Control;
+            this.button6.Location = new System.Drawing.Point(480, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 10);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "CLOSE";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gold;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(2, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(30, 10);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "MIN";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Green;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(35, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(30, 10);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Transparent;
+            this.button12.BackgroundImage = global::D_V_launcher.Properties.Resources.vd;
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Location = new System.Drawing.Point(458, 14);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(57, 50);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Transparent;
+            this.button11.BackgroundImage = global::D_V_launcher.Properties.Resources.cp;
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Location = new System.Drawing.Point(387, 14);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(67, 50);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Transparent;
+            this.button10.BackgroundImage = global::D_V_launcher.Properties.Resources.mail;
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(331, 14);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackgroundImage = global::D_V_launcher.Properties.Resources.pp;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(274, 14);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = global::D_V_launcher.Properties.Resources.word;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(218, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(162, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::D_V_launcher.Properties.Resources.vs;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(68, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 52);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::D_V_launcher.Properties.Resources.torch;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // D_V_Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(512, 68);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "D_V_Launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "D_V_launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ laptop/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+
+namespace laptop
+{
+    public partial class D_V_Launcher : Form
+    {
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        public bool checkstartup;
+        public D_V_Launcher()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+            this.button6.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button5.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button1.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button2.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button3.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button7.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button8.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button9.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button10.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button11.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button12.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button5.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button6.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button1.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button2.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button3.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button7.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button8.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button9.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button10.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button11.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button12.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            timer2.Enabled = true;
+            if (rkApp.GetValue("D_V_launcher") == null)
+            {
+                checkstartup = false;
+                this.button8.BackColor = System.Drawing.Color.Maroon;
+            }
+            else
+            {
+                checkstartup = true;
+                this.button8.BackColor = System.Drawing.Color.Green;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = .50;
+            this.Location = new Point((Screen.FromControl(this).Bounds.Width/2)-(ClientSize.Width/2), 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\name\AppData\Local\Torch\Application\torch.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+            
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            timer1.Enabled = true;
+            timer3.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Location = new Point((Screen.FromControl(this).Bounds.Width / 2) - (ClientSize.Width / 2), (Screen.FromControl(this).Bounds.Height) - (Screen.FromControl(this).Bounds.Height + 55));
+     
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Location = new Point((Screen.FromControl(this).Bounds.Width / 2) - (ClientSize.Width / 2), 0);
+   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files\Adobe\Adobe Photoshop CC 2014\Photoshop.exe");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files\Microsoft Office\Office15\WINWORD.EXE");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (this.TopMost == true)
+            { this.TopMost = false; }
+            else if(this.TopMost == false)
+            { this.TopMost = true; }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!checkstartup)
+            {
+                    rkApp.SetValue("D_V_launcher", Application.ExecutablePath.ToString());
+                    this.button8.BackColor = System.Drawing.Color.Green;
+                    checkstartup = true;
+            }
+            else if(checkstartup)
+            {
+                rkApp.DeleteValue("D_V_launcher", false);
+                this.button8.BackColor = Color.Maroon;
+                checkstartup = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\name\Desktop\past papers.exe");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start(@"C:\Users\name\Desktop\Mail");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\name\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Control Panel");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\VirtualDJ\virtualdj8.exe");
+        }
+    }
+}
